bb1fbaf11e0508333dab6b54d918289c10ba35c7
Nikhil Sharma <sharma28112000@gmail.com>
2022-07-20 06:29:31 +0530

    converted the f to Celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request= require('request')
 
 const forecast= (longitude,latitude, callback)=>{
-    const url = 'http://api.weatherstack.com//current?access_key=78637866e43d3d527761097e9e6cfca3&query='+ latitude +','+ longitude +'&units=f'
+    const url = 'http://api.weatherstack.com//current?access_key=78637866e43d3d527761097e9e6cfca3&query='+ latitude +','+ longitude +''
 
     request({url , json: true},(error,{body}) =>{
         if(error){
@@ -11,7 +11,7 @@ const forecast= (longitude,latitude, callback)=>{
             callback("Unable to connect to weather service", undefined)
         }
         else{
-            callback(undefined, body.current.weather_descriptions[0]+ ', It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.\n Humidity ='+ body.current.humidity) 
+            callback(undefined, body.current.weather_descriptions[0]+ ', It is currently ' + body.current.temperature + ' degrees celsius out. There is ' + body.current.precip + '% chance of rain.\n Humidity ='+ body.current.humidity) 
         }
     
     })
